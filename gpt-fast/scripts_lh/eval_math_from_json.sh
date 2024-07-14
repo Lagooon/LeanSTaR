5d63a404be635b863197f62e6696e81e4226816f
Noogal <2245869420@qq.com>
2024-07-13 20:28:49 -0400

    delete unused files

--- gpt-fast/scripts_lh/eval_math_from_json.sh
+++
@@ -1,9 +0,0 @@
-module load cuda/11.8
-# TMPDIR='/home/wliu/longhui/llms-all/alpaca-lora' pip install -r /home/wliu/longhui/llms-all/ScalableMath_sun-main/ScalableMath-1222/gpt-fast/re.txt
-export PATH=/home/wliu/anaconda3/envs/openrlhf/bin:$PATH
-SAVE_PATH='/lustre/fast/fast/wliu/longhui/trust_math_ckpt/checkpoints/llemma-7b_metamath-hf'
-python eval_math_vllm.py --model $SAVE_PATH --tensor_parallel_size 8 \
---data_file /home/wliu/longhui/llms-all/ScalableMath_sun-main/data/metamath_math_level1-3_20193_mapped.json \
---end 400
-echo $SAVE_PATH
-
--- gpt-fast/scripts_lh/improve_step_7b.sh
+++
@@ -1,3 +0,0 @@
-module load cuda/11.8
-export HF_HOME=/tmp
-bash /home/wliu/longhui/llms-all/ScalableMath_sun-main/ScalableMath-1228/gpt-fast/scripts_lh/finetune_7b_sft_metamath.sh
