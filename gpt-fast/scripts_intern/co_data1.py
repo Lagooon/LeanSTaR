af007e460840c4970d409b92fc435a796f729bb7
Lagooon <49017902+Lagooon@users.noreply.github.com>
2024-07-16 11:40:54 -0400

    Delete gpt-fast/scripts_intern/co_data1.py

--- gpt-fast/scripts_intern/co_data1.py
+++
@@ -1,9 +0,0 @@
-import json
-data = []
-with open(f"data/leandojo_benchmark_4/processed/STaR-generated-train.json", "r") as f:
-    data = data + json.load(f)
-with open(f"data/leandojo_benchmark_4/processed/gpt4-generated-train-1.json", "r") as f:
-    data = data + json.load(f)
-print(len(data))
-with open(f"data/leandojo_benchmark_4/processed/STaR-generated-train-1.json", "w") as f:
-    json.dump(data, f)
